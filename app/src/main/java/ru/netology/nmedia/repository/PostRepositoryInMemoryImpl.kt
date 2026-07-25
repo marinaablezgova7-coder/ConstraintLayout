80c8f9f40e86eed6027bc7c90e42cca4ca5d7a74
Neto test <marinaablezgova7@gmail.com>
2026-07-25 12:54:23 +0300

    Fix MVVM

--- app/src/main/java/ru/netology/nmedia/repository/PostRepositoryInMemoryImpl.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepositoryInMemoryImpl.kt
@@ -4,10 +4,10 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import ru.netology.nmedia.dto.Post
 
-class PostRepositoryInMemoryImpl: PostRepository {
+class PostRepositoryInMemoryImpl : PostRepository {
 
     var post = Post(
-        id =  1,
+        id = 1,
         author = "Нетология. Университет интернет-профессий будущего",
         published = "21 мая 18:36",
         content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
@@ -24,12 +24,14 @@ class PostRepositoryInMemoryImpl: PostRepository {
 
     override fun like() {
         post = post.copy(
-            likedByMe = !post.likedByMe, likes = (if (post.likedByMe) {
-                post.likes = 1
+            likedByMe = !post.likedByMe,
+            likes = if (post.likedByMe) {
+                post.likes - 1
             } else {
                 post.likes + 1
-            }) as Int
+            }
         )
+
         data.value = post
     }
 
